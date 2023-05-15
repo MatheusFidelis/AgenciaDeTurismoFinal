0d13595b086547d34864df455e96b1a8e6b9256b
Hesqueletico <47986805+Hesqueletico@users.noreply.github.com>
2023-05-15 15:50:20 -0300

    Criacao tela agencia

---
+++ tela/tela_agencia.py
@@ -0,0 +1,49 @@
+from tela.tela_abstrata import TelaAbstrata
+
+class TelaAgencia(TelaAbstrata):
+    def __init__(self):
+        pass
+    
+    def opcoes(self):   
+        print(" --- AGÊNCIA DE VIAGEM --- ")
+        print("Escolha uma das opções")
+        print("1 - Cliente (cadastre-se) ")
+        print("2 - Funcionario (cadastre-se)") 
+        print("3 - Acessar")
+        print("0 - Finalizar Sistema")
+        opcao = input("Escolha uma opção: ")
+        return opcao
+    
+    def acessar(self):
+        print(" --- AGÊNCIA DE VIAGEM --- ")
+        print("Escolha uma das opções")
+        print("1 - Sou Cliente")
+        print("2 - Sou Funcionario")
+        print("0 - voltar")
+        opcao = input("Escolha uma opção: ")
+        return opcao
+    
+    def tela_agencia_cliente(self):
+        print(" --- AGÊNCIA DE VIAGEM --- ")
+        print("Escolha uma das opções")
+        print("1 - Listar viagens disponíveis")
+        print("2 - Reservar")
+        print("3 - Listar Reservas")
+        print("0 - Voltar")
+        opcao = input("Escolha uma opção: ")
+        return opcao
+    
+    def total_vendas(self,total):
+        print(f"TOTAL DE VENDAS:  R$ {total}")
+    
+    def tela_agencia_funcionario(self):
+        print(" --- AGÊNCIA DE VIAGEM --- ")
+        print("Escolha uma das opções")
+        print("1 - Cadastrar Viagem")
+        print("2 - Listar reservas")
+        print("3 - Total de vendas")
+        print("0 - Voltar")
+        opcao = input("Escolha uma opcao: ")
+        return opcao
+    
+    
