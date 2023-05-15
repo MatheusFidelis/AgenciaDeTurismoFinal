060d8b3014b64713885b5b4d46a3bd792459e2ec
Matheus Fidélis <41090217+MatheusFidelis@users.noreply.github.com>
2023-05-15 15:58:17 -0300

    Modificacao na tela_agencia

--- tela/tela_agencia.py
+++ tela/tela_agencia.py
@@ -12,6 +12,7 @@ class TelaAgencia(TelaAbstrata):
         print("3 - Acessar")
         print("0 - Finalizar Sistema")
         opcao = input("Escolha uma opção: ")
+        print("\n")
         return opcao
     
     def acessar(self):
@@ -21,29 +22,32 @@ class TelaAgencia(TelaAbstrata):
         print("2 - Sou Funcionario")
         print("0 - voltar")
         opcao = input("Escolha uma opção: ")
+        print("\n")
         return opcao
     
     def tela_agencia_cliente(self):
-        print(" --- AGÊNCIA DE VIAGEM --- ")
+        print(" --- AGÊNCIA DE VIAGEM - CLIENTE --- ")
         print("Escolha uma das opções")
         print("1 - Listar viagens disponíveis")
         print("2 - Reservar")
         print("3 - Listar Reservas")
         print("0 - Voltar")
         opcao = input("Escolha uma opção: ")
+        print("\n")
         return opcao
     
     def total_vendas(self,total):
         print(f"TOTAL DE VENDAS:  R$ {total}")
     
     def tela_agencia_funcionario(self):
-        print(" --- AGÊNCIA DE VIAGEM --- ")
+        print(" --- AGÊNCIA DE VIAGEM - FUNCIONARIO --- ")
         print("Escolha uma das opções")
         print("1 - Cadastrar Viagem")
         print("2 - Listar reservas")
         print("3 - Total de vendas")
         print("0 - Voltar")
         opcao = input("Escolha uma opcao: ")
+        print("\n")
         return opcao
     
     
