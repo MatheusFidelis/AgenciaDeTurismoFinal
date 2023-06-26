ab5806d95d8a46a11a287cda5082927e8340e0c3
Matheus Fidélis <41090217+MatheusFidelis@users.noreply.github.com>
2023-06-26 17:40:53 -0300

    Update controlador_cliente.py

--- controlador/controlador_cliente.py
+++ controlador/controlador_cliente.py
@@ -1,38 +1,58 @@
 from controlador.controlador_abstrato import ControladorAbstrato
 from tela.tela_cliente import TelaCliente
 from entidade.cliente import Cliente
-
+from persistencia.cliente_dao import ClienteDAO
 
 class ControladorCliente(ControladorAbstrato):
+    __instance = None
     def __init__(self):
-        self.__clientes = []
+        self.__dao = ClienteDAO()
         self.__tela = TelaCliente()
+        
 
+    def __new__(cls):
+        if ControladorCliente.__instance is None:
+            ControladorCliente.__instance = object.__new__(cls)
+        return ControladorCliente.__instance
+    
     def inicia(self):
-        opcoes = {"1": self.cadastro, "2": self.alterar, "3": self.remover, "4": self.listar}
         while True:
             opcao = self.__tela.opcoes()
-            if opcao == "0":
+            if opcao == 0:
                 break
-            if opcao in opcoes.keys():
-                opcoes[opcao]()
-            else:
-                self.__tela.mensagem_erro("Comando Inválido")
-
+            elif opcao == 1:
+                self.cadastro()
+            elif opcao == 2:
+                self.alterar()
+            elif opcao == 3:
+                self.remover()
+            elif opcao == 4:
+                self.listar()
+    
+    
+    def encerra_sistema(self):
+        exit(1)
+    
     def cadastro(self):
+        
         while True:
             dados = self.__tela.dados_cliente()
+            if dados is None:
+                return self.encerra_sistema
             if not self.verificar_dados(dados):
                 continue
-            for cliente in self.__clientes:
+            cpf_duplicado = False
+            for cliente in self.__dao.get_all():
                 if cliente.cpf == int(dados["cpf"]):
                     self.__tela.mensagem_erro("Cpf duplicado")
+                    cpf_duplicado = True
                     continue
-            cliente = Cliente(dados["nome"], int(dados["cpf"]), int(dados["contato"]), dados["visto"])
-            self.__clientes.append(cliente)
-            self.__tela.mensagem("cadastro realizada com sucesso")
+            if not cpf_duplicado:
+                cliente = Cliente(dados["nome"], int(dados["cpf"]), int(dados["contato"] ),dados["visto"] )
+                self.__dao.add(cliente)
+                self.__tela.mensagem("cadastro realizada com sucesso")
             break
-
+            
     def verificar_dados(self, dados):
         if dados["nome"].strip() == "":
             self.__tela.mensagem_erro("Nome não pode estar vazio")
@@ -47,7 +67,7 @@ class ControladorCliente(ControladorAbstrato):
             self.__tela.mensagem_erro("Informe se possui visto")
             return False
         return True
-
+    
     def verificar_dados_alterar(self, dados):
         if dados["nome"].strip() == "":
             self.__tela.mensagem_erro("Nome não pode estar vazio")
@@ -59,7 +79,9 @@ class ControladorCliente(ControladorAbstrato):
             self.__tela.mensagem_erro("Informe se possui visto")
             return False
         return True
-
+    
+    
+        
     def verifica_cpf(self, cpf):
         if len(cpf) == 0:
             print("Cpf Invalido! Está vazio.")
@@ -72,26 +94,35 @@ class ControladorCliente(ControladorAbstrato):
                     print("O cpf não pode ser zero.")
             except ValueError:
                 print("Cpf Invalido!")
-
+    
     def dados_cliente(self, cliente):
-        dados = {"nome": cliente.nome, "cpf": cliente.cpf, "contato": cliente.contato, "visto": cliente.visto}
+        dados = {
+            "nome": cliente.nome,
+            "cpf": cliente.cpf,
+            "contato": cliente.contato,
+            "visto": cliente.visto
+        }
         return dados
-
+    
     def seleciona_cliente(self):
         cpf = self.__tela.seleciona()
-        for cliente in self.__clientes:
+        if cpf is None:
+            return None
+        for cliente in self.__dao.get_all():
             if cliente.cpf == int(cpf):
                 return cliente
-
-    def seleciona_cliente_externo(self, cpf):
-        for cliente in self.__clientes:
+    
+    def seleciona_cliente_externo(self,cpf):
+        
+        for cliente in self.__dao.get_all():
             if cliente.cpf == int(cpf):
                 return cliente
-
+    
     def alterar(self):
         cliente = self.seleciona_cliente()
-        if isinstance(cliente, Cliente):
-            self.__tela.mostra_cliente(self.dados_cliente(cliente))
+        
+        if isinstance (cliente, Cliente):
+            
             while True:
                 dados = self.__tela.dados_alterar()
                 dados["cpf"] = cliente.cpf
@@ -104,23 +135,29 @@ class ControladorCliente(ControladorAbstrato):
                 break
         else:
             self.__tela.mensagem_erro("Cliente não encontrado")
-
+        
+        
     def remover(self):
         cliente = self.seleciona_cliente()
+        
         if isinstance(cliente, Cliente):
-            self.__tela.mostra_cliente(self.dados_cliente(cliente))
-            confirmacao = self.__tela.confirma_exclusao()
-            if confirmacao == "0":
-                for i in range(len(self.__clientes)):
-                    if self.__clientes[i] == cliente:
-                        self.__clientes.pop(i)
-                        self.__tela.mensagem("Exclusão realizada com sucesso")
-                        break
+            confirmacao = self.__tela.confirma_exclusao_opcoes(self.dados_cliente(cliente))
+            if confirmacao == 'Confirmar':
+                self.__dao.remove(cliente.cpf)
+                self.__tela.mensagem("Exclusão realizada com sucesso")
             else:
                 self.__tela.mensagem("Exclusão cancelada com sucesso")
         else:
             self.__tela.mensagem_erro("Cliente não encontrado")
-
+    
+            
     def listar(self):
-        for cliente in self.__clientes:
-            self.__tela.mostra_cliente(self.dados_cliente(cliente))
+        
+        dados_clientes = []
+
+        for cliente in self.__dao.get_all():
+            dados_cliente = self.dados_cliente(cliente)
+            dados_clientes.append(dados_cliente)
+
+        self.__tela.mostra_cliente(dados_clientes)
+    
