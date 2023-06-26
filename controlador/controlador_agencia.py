c8a6683f5b6b71c77083aa9303b155ac6b05952f
Hesqueletico <47986805+Hesqueletico@users.noreply.github.com>
2023-06-26 17:34:42 -0300

    Update controlador_agencia.py
    Update no controlador_agencia

--- controlador/controlador_agencia.py
+++ controlador/controlador_agencia.py
@@ -9,54 +9,54 @@ from controlador.controlador_abstrato import ControladorAbstrato
 from controlador.controlador_viagem import ControladorViagem
 from controlador.controlador_funcionario import ControladorFuncionario
 from controlador.controlador_cliente import ControladorCliente
+from persistencia.reserva_dao import ReservaDAO
 
 class ControladorAgencia(ControladorAbstrato):
+    __instance = None
     def __init__(self):
-        self.__reservas = []
+        self.__dao = ReservaDAO()
         self.__tela_agencia = TelaAgencia()
         self.__tela_reserva = TelaReserva()
         self.__controlador_viagem = ControladorViagem()
         self.__controlador_funcionario = ControladorFuncionario()
         self.__controlador_cliente = ControladorCliente()
+    
+    def __new__(cls):
+        if ControladorAgencia.__instance is None:
+            ControladorAgencia.__instance = object.__new__(cls)
+        return ControladorAgencia.__instance
         
     def inicia(self): 
-        opcoes = {"1": self.controlador_cliente, "2": self.controlador_funcionario}
+        opcoes = {1: self.__controlador_cliente.inicia, 2: self.__controlador_funcionario.inicia, 3: self.acessar,
+                        0: self.encerra_sistema}
         while True:
             opcao = self.__tela_agencia.opcoes()
-            if opcao == "0":
-                break
-            if opcao == "3":
-                return self.acessar()
             if opcao in opcoes.keys():
-                opcoes[opcao].inicia()
-            else:
-                self.__tela.mensagem_erro("Comando Inválido")
+                opcoes[opcao]()
     
-    @property
+    
     def controlador_cliente(self):
         return self.__controlador_cliente
     
     @property
     def controlador_funcionario(self):
         return self.__controlador_funcionario
+    
+    
+    
     @property
     def controlador_viagem(self):
         return self.__controlador_viagem
     
+    def encerra_sistema(self):
+        exit(1)
+    
     def acessar(self):
+        opcoes = {1: self.cliente, 2: self.funcionario, 0: self.inicia}
         while True:
-            opcao = self.__tela_agencia.acessar()
-            if opcao == "0":
-                self.inicia()
-            try:
-                if opcao == "1":
-                    return self.cliente()
-                elif opcao == "2":
-                    return self.funcionario()
-                else:
-                    raise ValueError
-            except ValueError:
-                self.__tela_agencia.mensagem_erro("Comando Inválido")
+            opcao_escolhida = self.__tela_agencia.acessar()
+            funcao_escolhida = opcoes[opcao_escolhida]
+            funcao_escolhida()
         
 
     
@@ -70,48 +70,38 @@ class ControladorAgencia(ControladorAbstrato):
     def agencia_cliente(self):
         while True:
             opcao = self.__tela_agencia.tela_agencia_cliente()
-            if opcao == "0":
-                return self.acessar()
-            if opcao == "1":
+            if opcao == 0:
+                break
+            if opcao == 1:
                 self.listar_viagens_disp()
-            elif opcao == "2":
+            elif opcao == 2:
                 self.reservar()
-            elif opcao == "3":
+            elif opcao == 3:
                 self.listar_reservas_feitas()
-            else:
-                self.__tela_agencia.mensagem_erro("Comando Inválido")
+            
         
     
     def listar_viagens_disp(self):
-        viagens_disp = []
-        codigos_reservas = [reserva.codigo for reserva in self.__reservas]  # Obter os códigos de todas as reservas
+        self.__controlador_viagem.listar_viagens_com_vagas()
 
-        for viagem in self.__controlador_viagem.viagens:
-            codigo_viagem = viagem.codigo
-            if codigo_viagem not in codigos_reservas:  # Verificar se o código da viagem não está na lista de códigos de reservas
-                viagens_disp.append(viagem)
-
-        for viagem in viagens_disp:
-            if isinstance(viagem, ViagemInternacional):
-                self.__controlador_viagem._ControladorViagem__tela.mostrar_internacional(self.__controlador_viagem.dados_viagem_internacional(viagem))
-            elif isinstance(viagem, ViagemNacional):
-                self.__controlador_viagem._ControladorViagem__tela.mostrar_nacional(self.__controlador_viagem.dados_viagem_nacional(viagem))
 
     
                     
     def seleciona_reserva(self):
         codigo = self.__tela_reserva.seleciona()
-        for reserva in self.__reservas:
+        for reserva in self.__dao.get_all():
             if reserva.codigo == int(codigo):
                 return reserva
             
     def reservar(self):
         dados_reserva = self.__tela_reserva.dados_reserva()
+        if dados_reserva is None:
+                return self.encerra_sistema
 
-        codigo = dados_reserva["codigo"]
+        codigo = len(self.__dao.get_all()) + 1
         # Verificar se já existe uma reserva com o mesmo código
-        if any(reserva.codigo == codigo for reserva in self.__reservas):
-            self.__tela_reserva.mensagem_erro("Já existe uma reserva com esse código.")
+        if any(reserva.codigo == codigo for reserva in self.__dao.get_all()):
+            codigo += 1
             return
 
         viagem = self.__controlador_viagem.seleciona_externo(codigo)
@@ -145,18 +135,27 @@ class ControladorAgencia(ControladorAbstrato):
         if visto and not cliente.visto:
             return self.__tela_reserva.mensagem_erro("O cliente não possui visto válido para esta viagem.")
         
+        
         reserva = Reserva(codigo, cliente, nome_cliente, viagem, pais, origem, destino, descricao, ida, volta, data_reserva, qtd_vagas, n_pessoas, preco_unitario, tipo, visto, valor_final)
-        self.__reservas.append(reserva)
+        self.__dao.add(reserva)
+        
+        viagem.qtd_vagas -= n_pessoas
 
         self.__tela_reserva.mensagem("Reserva cadastrada com sucesso.")
             
     def listar_reservas_feitas(self):
-        if not self.__reservas:
+        if not self.__dao.get_all():
             self.__tela_agencia.mensagem("Não há reservas cadastradas.")
             return self.agencia_cliente()
-        for reserva in self.__reservas:
-            self.__tela_reserva.mostrar_reserva(self.dados_reserva(reserva))
-                
+
+        dados_reservas = []
+
+        for reserva in self.__dao.get_all():
+            dados_reserva = self.dados_reserva(reserva)
+            dados_reservas.append(dados_reserva)
+
+        self.__tela_reserva.mostrar_reserva(dados_reservas)
+                    
         
     def dados_reserva(self,reserva):
         dados = {"codigo": reserva.codigo, "cliente": reserva.cliente,"nome_cliente": reserva.nome_cliente, "viagem": reserva.viagem,"pais":reserva.pais, "origem":reserva.origem, "destino":reserva.destino ,"descricao":reserva.descricao, "ida":reserva.ida, "volta":reserva.volta,"data_reserva": reserva.data_reserva, "qtd_vagas":reserva.qtd_vagas,"n_pessoas": reserva.n_pessoas,"preco_unitario":reserva.preco_unitario,"tipo":reserva.tipo,"visto":reserva.visto,"valor_final": reserva.valor_final }
@@ -168,26 +167,24 @@ class ControladorAgencia(ControladorAbstrato):
         
         if isinstance(funcionario, Funcionario):
             self.__tela_agencia.mensagem("Acesso permitido")
-            opcoes = {"1": self.controlador_viagem}
+            opcoes = {1: self.controlador_viagem}
             while True:
                 opcao = self.__tela_agencia.tela_agencia_funcionario()
-                if opcao == "0":
+                if opcao == 0:
                     self.acessar()
-                elif opcao == "2":
+                elif opcao == 2:
                     return self.listar_reservas_feitas()
-                elif opcao == "3":
+                elif opcao == 3:
                     return self.total_vendas()
                 elif opcao in opcoes.keys():
                     opcoes[opcao].inicia()
-                else:
-                    self.__tela_agencia.mensagem_erro("Comando Inválido")
         else:
             self.__tela_agencia.mensagem_erro("Acesso negado")
             self.acessar()
             
     def total_vendas(self):
         total = 0
-        for reserva in self.__reservas:
+        for reserva in self.__dao.get_all():
             total += reserva.valor_final
         self.__tela_agencia.total_vendas(total)
         self.acessar()
