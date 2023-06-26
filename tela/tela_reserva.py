d332adeef70966f3de0db78d1fb661b2e2047510
Hesqueletico <47986805+Hesqueletico@users.noreply.github.com>
2023-06-26 17:35:52 -0300

    Update tela_reserva.py

--- tela/tela_reserva.py
+++ tela/tela_reserva.py
@@ -1,35 +1,74 @@
+import PySimpleGUI as sg
 from tela.tela_abstrata import TelaAbstrata
 import datetime
 
 class TelaReserva(TelaAbstrata):
     def __init__(self):
-        pass
+        self.__window = None
+    
+    def open(self):
+        button, values = self.__window.Read()
+        return button, values
+    
+    def close(self):
+        self.__window.Close()
     
     def mostrar_reserva(self,dados):
-        print(" --- RESERVAS --- ")
-        print("Codigo: ", dados["codigo"])
-        print("Cliente: ", dados["nome_cliente"])
-        print("País: ", dados["pais"])
-        print("Origem: ", dados["origem"])
-        print("Destino: ", dados["destino"])
-        print("Descrição: ", dados["descricao"])
-        print("Ida: ", dados["ida"])
-        print("Volta: ",dados["volta"])
-        print("Data da Reserva: ",dados["data_reserva"])
-        print("Número de Vagas Permitido: ",dados["qtd_vagas"])
-        print("Número de Vagas Preenchidas: ",dados["n_pessoas"])
-        print("Preço Unitario: ",dados["preco_unitario"])
-        print("Total: ",dados["valor_final"])
-        print("\n")
-    
+        data = []
+
+        for reserva in dados:
+            reserva_data = [
+                ["Codigo: ", reserva["codigo"]],
+                ["Cliente: ", reserva["nome_cliente"]],
+                ["País: ", reserva["pais"]],
+                ["Origem: ", reserva["origem"]],
+                ["Destino: ", reserva["destino"]],
+                ["Descrição: ", reserva["descricao"]],
+                ["Ida: ", reserva["ida"]],
+                ["Volta: ", reserva["volta"]],
+                ["Data da Reserva: ", reserva["data_reserva"]],
+                ["Número de Vagas: ", reserva["qtd_vagas"]],
+                ["Número de Vagas Preenchidas: ", reserva["n_pessoas"]],
+                ["Preço Unitario: ", reserva["preco_unitario"]],
+                ["Total: ", reserva["valor_final"]],
+                ["#####################"],
+                ["\n"]
+            ]
+            data.extend(reserva_data)
+        header = ["Dados","Reserva"]
+        sg.ChangeLookAndFeel('DarkBlue')
+
+        layout = [
+            [sg.Text(' ------ Listar ------ ', pad=((150, 100), (0, 0)),justification='center', font=("Helvetica", 25))],
+            [sg.Table(values=data, headings=header, pad=((100, 100), (0, 0)),  alternating_row_color='DarkSlateGray',justification='center')],
+            [sg.Cancel('Voltar',  pad=((250, 50), (0, 0)),button_color=('white', 'black'))]
+        ]
+        self.__window = sg.Window('AGÊNCIA DE VIAGEM').Layout(layout)
+        
+        button, values = self.open()
+        if button in (None, 'Voltar'):
+            self.close()
+            return None
+        self.close()
         
     
     def dados_reserva(self):
-        print(" --- RESERVA --- ")
-        codigo = input("codigo da viagem: ")
-        cliente = input("cpf do cliente: ")
+        sg.ChangeLookAndFeel('DarkBlue')
+        layout = [
+        [sg.Text(' ------ RESERVA ------ ',pad=((100, 100), (0, 0)),justification='center', font=("Helvica", 25))],
+        [sg.Text('Codigo da viagem:', size=(15, 1)), sg.InputText('', key='viagem')],
+        [sg.Text('Cpf do cliente:', size=(15, 1)), sg.InputText('', key='cliente')],
+        [sg.Text('Quantas pessoas:', size=(15, 1)), sg.InputText('', key='n_pessoas')],
+        [sg.Button('Confirmar', button_color=('white', 'black')), sg.Cancel('Cancelar', button_color=('white', 'black'))]
+        ]
+        self.__window = sg.Window('AGÊNCIA DE VIAGEM').Layout(layout)
+        
+        button, values = self.open()
+        
+        codigo = int
+        cliente = values['cliente']
         nome_cliente = str
-        viagem = None
+        viagem = values['viagem']
         pais = str
         origem = str
         destino = str
@@ -38,16 +77,31 @@ class TelaReserva(TelaAbstrata):
         volta = datetime
         data_reserva = datetime
         qtd_vagas = None
-        n_pessoas = int(input("Reserva para quantas pessoas: "))
+        n_pessoas = values['n_pessoas']
         preco_unitario = float
         tipo = str
         visto = False
         valor_final = float
-        print("\n")
+        if button in (None, 'Cancelar'):
+            self.close()
+            return None
+
+        self.close()
+        
         return {"codigo": codigo, "cliente": cliente,"nome_cliente":nome_cliente, "viagem": viagem,"pais":pais, "origem":origem, "destino":destino ,"descricao":descricao, "ida":ida, "volta":volta,"data_reserva": data_reserva, "qtd_vagas":qtd_vagas,"n_pessoas": n_pessoas,"preco_unitario":preco_unitario,"tipo":tipo,"visto":visto,"valor_final": valor_final }
     
     def seleciona(self):
-        print("------- SELECIONAR RESERVA -------")
-        codigo = input("codigo: ")
-        print("\n")
+        sg.ChangeLookAndFeel('DarkBlue')
+        layout = [
+        [sg.Text(' ------ SELECIONAR v ------ ', font=("Helvica", 25))],
+        [sg.Text('codigo:', size=(8, 1)), sg.InputText('', key='codigo')],
+        [sg.Button('Confirmar', button_color=('white', 'black')), sg.Cancel('Cancelar', button_color=('white', 'black'))]
+        ]
+        self.__window = sg.Window('AGÊNCIA DE VIAGEM').Layout(layout)
+        button, values = self.open()
+        codigo = values['codigo']
+        if button in (None, 'Cancelar'):
+            self.close()
+            return None
+        self.close()
         return codigo
