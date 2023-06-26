58cf77538fd9c24426d0dc5ea7ae8291b6c6429e
Matheus Fidélis <41090217+MatheusFidelis@users.noreply.github.com>
2023-06-26 17:47:10 -0300

    Update tela_agencia.py

--- tela/tela_agencia.py
+++ tela/tela_agencia.py
@@ -1,53 +1,151 @@
+import PySimpleGUI as sg
 from tela.tela_abstrata import TelaAbstrata
 
 class TelaAgencia(TelaAbstrata):
     def __init__(self):
-        pass
+        self.__window = None
+        self.init_opcoes()
+    
+    
+    def init_opcoes(self):
+    #sg.theme_previewer()
+        sg.ChangeLookAndFeel('DarkBlue')
+        layout = [
+        [sg.Text(' ------ Agência de Viagem ------ ', font=("Helvica", 25))],
+        [sg.Text('Escolha sua opção', font=("Helvica", 15))],
+        [sg.Radio('Cliente (cadastre-se)', "RD1", key='1')],
+        [sg.Radio('Funcionario (cadastre-se)', "RD1", key='2')],
+        [sg.Radio('Acessar', "RD1", key='3')],
+        [sg.Radio('Finalizar Sistema', "RD1", key='0')],
+        [sg.Button('Confirmar', button_color=('white', 'black'))]
+        ]
+        self.__window = sg.Window('AGÊNCIA DE VIAGEM').Layout(layout)
+    
+    def open(self):
+        button, values = self.__window.Read()
+        return button, values
+    
+    def close(self):
+        self.__window.Close()
     
     def opcoes(self):   
-        print(" --- AGÊNCIA DE VIAGEM --- ")
-        print("Escolha uma das opções")
-        print("1 - Cliente (cadastre-se) ")
-        print("2 - Funcionario (cadastre-se)") 
-        print("3 - Acessar")
-        print("0 - Finalizar Sistema")
-        opcao = input("Escolha uma opção: ")
-        print("\n")
+        self.init_opcoes()
+        button, values = self.open()
+        if values['1']:
+            opcao = 1
+        if values['2']:
+            opcao = 2
+        if values['3']:
+            opcao = 3
+        # cobre os casos de Retornar, fechar janela, ou clicar cancelar
+        #Isso faz com que retornemos a tela do sistema caso qualquer uma dessas coisas aconteca
+        if values['0']:
+            opcao = 0
+        self.close()
         return opcao
+    
+    
+    
+    def acessar_opcoes(self):
+        sg.ChangeLookAndFeel('DarkBlue')
+        layout = [
+        [sg.Text(' ------ Agência de Viagem ------ ', font=("Helvica", 25))],
+        [sg.Text('Escolha sua opção', font=("Helvica", 15))],
+        [sg.Radio('Sou Cliente', "RD1", key='1')],
+        [sg.Radio('Sou Funcionario', "RD1", key='2')],
+        [sg.Radio('Voltar', "RD1", key='0')],
+        [sg.Button('Confirmar', button_color=('white', 'black'))]
+        ]
+        self.__window = sg.Window('AGÊNCIA DE VIAGEM').Layout(layout)
+    
     
     def acessar(self):
-        print(" --- AGÊNCIA DE VIAGEM --- ")
-        print("Escolha uma das opções")
-        print("1 - Sou Cliente")
-        print("2 - Sou Funcionario")
-        print("0 - voltar")
-        opcao = input("Escolha uma opção: ")
-        print("\n")
+        self.acessar_opcoes()
+        button, values = self.open()
+        if values['1']:
+            opcao = 1
+        if values['2']:
+            opcao = 2
+        # cobre os casos de Retornar, fechar janela, ou clicar cancelar
+        #Isso faz com que retornemos a tela do sistema caso qualquer uma dessas coisas aconteca
+        if values['0']:
+            opcao = 0
+        self.close()
         return opcao
+
+    
+    def tela_agencia_cliente_opcoes(self):
+        sg.ChangeLookAndFeel('DarkBlue')
+        layout = [
+        [sg.Text(' ------ AGÊNCIA DE VIAGEM - CLIENTE ------ ', font=("Helvica", 25))],
+        [sg.Text('Escolha sua opção', font=("Helvica", 15))],
+        [sg.Radio('Listar viagens disponíveis', "RD1", key='1')],
+        [sg.Radio('Reservar', "RD1", key='2')],
+        [sg.Radio('Listar Reservas', "RD1", key='3')],
+        [sg.Radio('Voltar', "RD1", key='0')],
+        [sg.Button('Confirmar', button_color=('white', 'black'))]
+        ]
+        self.__window = sg.Window('AGÊNCIA DE VIAGEM').Layout(layout)
     
     def tela_agencia_cliente(self):
-        print(" --- AGÊNCIA DE VIAGEM - CLIENTE --- ")
-        print("Escolha uma das opções")
-        print("1 - Listar viagens disponíveis")
-        print("2 - Reservar")
-        print("3 - Listar Reservas")
-        print("0 - Voltar")
-        opcao = input("Escolha uma opção: ")
-        print("\n")
+        self.tela_agencia_cliente_opcoes()
+        button, values = self.open()
+        if values['1']:
+            opcao = 1
+        if values['2']:
+            opcao = 2
+        if values['3']:
+            opcao = 3
+        # cobre os casos de Retornar, fechar janela, ou clicar cancelar
+        #Isso faz com que retornemos a tela do sistema caso qualquer uma dessas coisas aconteca
+        if values['0']:
+            opcao = 0
+        self.close()
         return opcao
+
     
     def total_vendas(self,total):
-        print(f"TOTAL DE VENDAS:  R$ {total}")
+        sg.ChangeLookAndFeel('DarkBlue')
+        layout = [
+        [sg.Text(' ------ TOTAL DE VENDAS ------ ', font=("Helvica", 25))],
+        [sg.Text(f'TOTAL DE VENDAS:  R$ {total}', font=("Helvica", 15))],        
+        [sg.Cancel('Voltar', button_color=('white', 'black'))]
+        ]
+        self.__window = sg.Window('AGÊNCIA DE VIAGEM').Layout(layout)
+        button, values = self.open()
+        if button in (None, 'Cancelar'):
+            self.close()
+            return None
+        self.close()
+    
+    def tela_agencia_funcionario_opcoes(self):
+        sg.ChangeLookAndFeel('DarkBlue')
+        layout = [
+        [sg.Text(' ------ AGÊNCIA DE VIAGEM - FUNCIONARIO ------ ', font=("Helvica", 25))],
+        [sg.Text('Escolha sua opção', font=("Helvica", 15))],
+        [sg.Radio('Cadastrar Viagem', "RD1", key='1')],
+        [sg.Radio('Listar Reserva', "RD1", key='2')],
+        [sg.Radio('Total de vendas', "RD1", key='3')],
+        [sg.Radio('Voltar', "RD1", key='0')],
+        [sg.Button('Confirmar', button_color=('white', 'black'))]
+        ]
+        self.__window = sg.Window('AGÊNCIA DE VIAGEM').Layout(layout)
     
     def tela_agencia_funcionario(self):
-        print(" --- AGÊNCIA DE VIAGEM - FUNCIONARIO --- ")
-        print("Escolha uma das opções")
-        print("1 - Cadastrar Viagem")
-        print("2 - Listar reservas")
-        print("3 - Total de vendas")
-        print("0 - Voltar")
-        opcao = input("Escolha uma opcao: ")
-        print("\n")
+        self.tela_agencia_funcionario_opcoes()
+        button, values = self.open()
+        if values['1']:
+            opcao = 1
+        if values['2']:
+            opcao = 2
+        if values['3']:
+            opcao = 3
+        # cobre os casos de Retornar, fechar janela, ou clicar cancelar
+        #Isso faz com que retornemos a tela do sistema caso qualquer uma dessas coisas aconteca
+        if values['0']:
+            opcao = 0
+        self.close()
         return opcao
+
     
     
